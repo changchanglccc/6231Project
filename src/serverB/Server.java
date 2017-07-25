252fb2fc1c0396d968d54261d46f72afb5c71a6d
Tann <Tianen.chen@outlook.com>
2017-07-25 12:31:29 -0400

    structure

---
+++ src/serverB/Server.java
@@ -0,0 +1,8 @@
+package serverB;
+
+public class Server {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/serverC/Server.java
@@ -0,0 +1,8 @@
+package serverC;
+
+public class Server {
+
+    public static void main(String[] args){
+
+    }
+}
