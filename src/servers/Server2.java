f8c33a2836d0771fab9309b5924211fb3cbc7e6f
Grey <GreyLee369@gmail.com>
2017-07-29 10:21:45 -0400

    "Servers"

---
+++ src/servers/Server2.java
@@ -0,0 +1,17 @@
+package servers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import records.Record;
+import sun.misc.Queue;
+
+public class Server2 {
+	private HashMap<Character,ArrayList<Record>> DDOServer2;
+	private HashMap<Character,ArrayList<Record>> MTLServer2;
+	private HashMap<Character,ArrayList<Record>> LVLServer2;
+	private Queue<String> queue;
+    private File loggingFile;
+
+}
---
+++ src/servers/Server3.java
@@ -0,0 +1,17 @@
+package servers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import records.Record;
+import sun.misc.Queue;
+
+public class Server3 {
+	private HashMap<Character,ArrayList<Record>> DDOServer3;
+	private HashMap<Character,ArrayList<Record>> MTLServer3;
+	private HashMap<Character,ArrayList<Record>> LVLServer3;
+	private Queue<String> queue;
+    private File loggingFile;
+
+}
