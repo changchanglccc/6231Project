8b69f8066911b07a1a950e820b634fbd9d2336dd
Grey <GreyLee369@gmail.com>
2017-08-05 21:19:41 -0400

    "udp listeners"

--- src/thread/UdpListener2.java
+++ src/thread/UdpListener2.java
@@ -3,23 +3,18 @@ package thread;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-
-import servers.CommonServer;
 import servers.Server2;
 
 public class UdpListener2 extends Thread{
 
-    private String message;
+	private String message;
     private int port;
-    private CommonServer commonServer;
     private Server2 server2;
     private boolean flag;
 	private String replyMessage;
 
-    public UdpListener2(CommonServer server, int portNumber,Server2 server2){
+    public UdpListener2(int portNumber,Server2 server2){
         this.port=portNumber;
-        this.commonServer = server;
         this.server2 = server2;
         this.message = "";
         this.flag = false;
@@ -34,74 +29,77 @@ public class UdpListener2 extends Thread{
 
 	@Override
     public void run() {
-        DatagramSocket datagramSocket = null;
-        try {
-            //create belonging socket
-            datagramSocket = new DatagramSocket(port);
-            byte[] buffer = new byte[1000];
-            byte[] reply = new byte[1000];
-//            System.out.println(centerServerImp.centerName+"is ready to listen UDP requests between servers");
-            //listening
-            while(true){
-//            	message="";
-                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                datagramSocket.receive(request);
-                message=new String(request.getData());
-                System.out.println("updListener: "+getMessage());
-                commonServer.setMessage(message);
+		 DatagramSocket datagramSocket = null;
+	        try {
+	            //create belonging socket
+	            datagramSocket = new DatagramSocket(port);
+	            byte[] buffer = new byte[1000];
+	            byte[] reply = new byte[1000];
+//	            System.out.println(centerServerImp.centerName+"is ready to listen UDP requests between servers");
+	            //listening
+	            while(true){
+	            	message="";
+	                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+	                datagramSocket.receive(request);
+	                message=new String(request.getData());
+	                System.out.println("updListener: "+getMessage());
+//	                commonServer.setMessage(message);
+	                server2.setMessage(message);
 
-                
-                if(!message.equals("")){
-    		    	String[] strings = message.split(",");
-    		    	switch(strings[0]){
-    		    		case "1":
-    		    			flag = server2.createTRecord(strings[1], strings[2], strings[3], strings[4], strings[5], strings[6], strings[7]);
-    		    			server2.multicast(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "2":
-    		    			flag = server2.createSRecord(strings[1], strings[2], strings[3], strings[4], strings[5], strings[6]);
-    		    			server2.multicast(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "3":
-    		    			replyMessage = server2.getRecordCounts(strings[1]);
-    		    			reply = replyMessage.getBytes();
-    		    			break;
-    		    		case "4":
-    		    			flag = server2.editRecord(strings[1], strings[2], strings[3], strings[4]);
-    		    			server2.multicast(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "5":
-    		    			flag = server2.transferRecord(strings[1], strings[2], strings[3]);
-    		    			server2.multicast(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "7":
-    		    			replyMessage = server2.getRecordInfo(strings[1],strings[2]);
-    		    			reply = replyMessage.getBytes();
-    		    			break;
-    		    		default:
-    		    			System.out.println("error!");
-    		    	}
-//    		    	message="";
-    		    	
-    				try {
-    					DatagramPacket replyPacket = new DatagramPacket(reply, reply.length, request.getAddress(), request.getPort());
-    					datagramSocket.send(replyPacket);
-    					
-    				} catch (IOException e) {
-    					// TODO Auto-generated catch block
-    					e.printStackTrace();
-    				}
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }finally {
-            if(datagramSocket != null)
-                datagramSocket.close();
-        }
+	                
+	                if(!message.equals("")){
+	    		    	String[] strings = message.split(",");
+	    		    	switch(strings[0]){
+	    		    		case "1":
+	    		    			flag = server2.createTRecord(strings[1], strings[2], strings[3], strings[4], strings[5], strings[6], strings[7]);
+	    		    			server2.multicast2(message);
+	    		    			reply = String.valueOf(flag).getBytes();
+	    		    			break;
+	    		    		case "2":
+	    		    			flag = server2.createSRecord(strings[1], strings[2], strings[3], strings[4], strings[5], strings[6]);
+	    		    			server2.multicast2(message);
+	    		    			reply = String.valueOf(flag).getBytes();
+	    		    			break;
+	    		    		case "3":
+	    		    			replyMessage = server2.getRecordCounts(strings[1]);
+	    		    			reply = replyMessage.getBytes();
+	    		    			break;
+	    		    		case "4":
+	    		    			flag = server2.editRecord(strings[1], strings[2], strings[3], strings[4]);
+	    		    			server2.multicast2(message);
+	    		    			reply = String.valueOf(flag).getBytes();
+	    		    			break;
+	    		    		case "5":
+	    		    			flag = server2.transferRecord(strings[1], strings[2], strings[3]);
+	    		    			server2.multicast2(message);
+	    		    			reply = String.valueOf(flag).getBytes();
+	    		    			break;
+	    		    		case "7":
+	    		    			replyMessage = server2.getRecordInfo(strings[1],strings[2]);
+	    		    			reply = replyMessage.getBytes();
+	    		    			break;
+	    		    		default:
+	    		    			System.out.println("error!");
+	    		    	}
+//	    		    	message="";
+	    		    	
+	    				try {
+	    					DatagramPacket replyPacket = new DatagramPacket(reply, reply.length, request.getAddress(), request.getPort());
+	    					datagramSocket.send(replyPacket);
+	    					buffer = new byte[1000];// clear buffer, avoid the buffer last time influences the receiving buffer 
+	    					reply = new byte[1000];
+	    					
+	    				} catch (IOException e) {
+	    					// TODO Auto-generated catch block
+	    					e.printStackTrace();
+	    				}
+	                }
+	            }
+	        } catch (Exception e) {
+	            System.out.println(e.getMessage());
+	        }finally {
+	            if(datagramSocket != null)
+	                datagramSocket.close();
+	        }
     }
 }
--- src/thread/UdpListener3.java
+++ src/thread/UdpListener3.java
@@ -1,8 +1,5 @@
 package thread;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import servers.Server3;
 
 public class UdpListener3 extends Thread{
@@ -29,77 +26,6 @@ public class UdpListener3 extends Thread{
 
 	@Override
     public void run() {
-        DatagramSocket datagramSocket = null;
-        try {
-            //create belonging socket
-            datagramSocket = new DatagramSocket(port);
-            byte[] buffer = new byte[1000];
-            byte[] reply = new byte[1000];
-//            System.out.println(centerServerImp.centerName+"is ready to listen UDP requests between servers");
-            //listening
-            while(true){
-            	message="";
-                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                datagramSocket.receive(request);
-                message=new String(request.getData());
-                System.out.println("updListener: "+getMessage());
-//                commonServer.setMessage(message);
-                server3.setMessage(message);
-
-                
-                if(!message.equals("")){
-    		    	String[] strings = message.split(",");
-    		    	switch(strings[0]){
-    		    		case "1":
-    		    			flag = server3.createTRecord(strings[1], strings[2], strings[3], strings[4], strings[5], strings[6], strings[7]);
-    		    			server3.multicast2(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "2":
-    		    			flag = server3.createSRecord(strings[1], strings[2], strings[3], strings[4], strings[5], strings[6]);
-    		    			server3.multicast2(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "3":
-    		    			replyMessage = server3.getRecordCounts(strings[1]);
-    		    			reply = replyMessage.getBytes();
-    		    			break;
-    		    		case "4":
-    		    			flag = server3.editRecord(strings[1], strings[2], strings[3], strings[4]);
-    		    			server3.multicast2(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "5":
-    		    			flag = server3.transferRecord(strings[1], strings[2], strings[3]);
-    		    			server3.multicast2(message);
-    		    			reply = String.valueOf(flag).getBytes();
-    		    			break;
-    		    		case "7":
-    		    			replyMessage = server3.getRecordInfo(strings[1],strings[2]);
-    		    			reply = replyMessage.getBytes();
-    		    			break;
-    		    		default:
-    		    			System.out.println("error!");
-    		    	}
-//    		    	message="";
-    		    	
-    				try {
-    					DatagramPacket replyPacket = new DatagramPacket(reply, reply.length, request.getAddress(), request.getPort());
-    					datagramSocket.send(replyPacket);
-    					buffer = new byte[1000];// clear buffer, avoid the buffer last time influences the receiving buffer 
-    					reply = new byte[1000];
-    					
-    				} catch (IOException e) {
-    					// TODO Auto-generated catch block
-    					e.printStackTrace();
-    				}
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }finally {
-            if(datagramSocket != null)
-                datagramSocket.close();
-        }
+       
     }
 }
