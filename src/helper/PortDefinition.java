1f19fb354cd9004a1ee7705155b6d8074a03c6a7
Grey <GreyLee369@gmail.com>
2017-08-10 21:21:22 -0400

    "Helper"

---
+++ src/helper/PortDefinition.java
@@ -0,0 +1,26 @@
+package helper;
+
+public class PortDefinition {
+
+    public static final int FE_ACKOWLEDGE_PORT=4000;
+    public static final int FE_OPEARION_PORT=5000;
+    public static final int FE_PRIMARY=4888;
+    public static final int FailureDetector=4999;
+    public static final int FE_INITIAL_PORT=4777;
+
+
+    public static final int S1_ACKOWLEDGE_PORT=4001;
+    public static final int S1_OPEARION_PORT=5001;
+    public static final int S1_ELECTION_PORT=6001;
+
+
+    public static final int S2_ACKOWLEDGE_PORT=4002;
+    public static final int S2_OPEARION_PORT=5002;
+    public static final int S2_ELECTION_PORT=6002;
+
+
+    public static final int S3_ACKOWLEDGE_PORT=4003;
+    public static final int S3_OPEARION_PORT=5003;
+    public static final int S3_ELECTION_PORT=6003;
+
+}
